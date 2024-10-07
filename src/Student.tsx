ce479b01b699e288ebd95ff67aaf461944b7202e
GASTON LARRIERA <larriera.gaston@gmail.com>
2024-10-06 21:09:06 -0300

    Modularize filters and table & combine filters

--- src/Student.tsx
+++ src/Student.tsx
@@ -3,14 +3,22 @@ import type {FC} from "react"
 type StudentAttendance = "Regular" | "Libre"
 type StudentCanPromote = "S" | "N"
 
-export interface StudentData {
+export interface StudentProps {
   idNumber: number,
   name: string,
   attendance:  StudentAttendance,
   can_promote: StudentCanPromote
 }
 
-export const Student: FC<StudentData> = ({idNumber, name, attendance}) => (
-    <li key={idNumber}>Nombre: {name}, Condición: {attendance}</li>
-  )
-
+export const Student: FC<StudentProps> = ({idNumber, name, attendance, can_promote}) => (
+  <tr key={idNumber}>
+    <td>{idNumber}</td>
+    <td>{name}</td>
+    <td>{attendance}</td>
+    <td>{can_promote}</td>
+    <td>
+      <button onClick={() => alert(`Aqui se debería editar el alumno ${idNumber}`)}>Editar</button>
+      <button onClick={() => alert(`Aqui se debería borrar el alumno ${idNumber}`)}>Borrar</button>
+    </td>
+  </tr>
+)
--- src/StudentsList.tsx
+++ src/StudentsList.tsx
@@ -1,15 +1,17 @@
 import './App.css'
-import studentsJSON from '../students-2024.json'
 import { useState, useEffect } from 'react'
 import type {FC} from 'react'
-import { Student, StudentData } from './Student'
+import { StudentProps } from './Student'
+import { StudentsFilters } from './StudentsFilters';
+import { StudentsTable } from './StudentsTable';
+import studentsJSON from '../students-2024.json';
 
 export const StudentsList: FC = () => {
-  const students = studentsJSON as Array<StudentData>
+  const students = studentsJSON as Array<StudentProps>
 
   const [isLoading, setIsLoading] = useState(false)
   const [isFiltered, setIsFiltered] = useState(false)
-  const [filteredStudents, setFilteredStudents] = useState<Array<StudentData>>([])
+  const [filteredStudents, setFilteredStudents] = useState<Array<StudentProps>>([])
   const [searchText, setSearchText] = useState<string>('')
 
   useEffect(() => {
@@ -23,44 +25,26 @@ export const StudentsList: FC = () => {
     return () => clearTimeout(timer)
   }, [])
 
-  const onFilterClick = () => {
-    // if(isFiltered){
-    //   setFilteredStudents(students)
-    // } else {
-    //   setFilteredStudents(students.filter(student => student.attendance === "Regular"))
-    // }
-    setFilteredStudents(students.filter(student => isFiltered || student.attendance === "Regular"))
-    setIsFiltered(prevValue => !prevValue)
-  }
 
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> | undefined = (e) => { 
-    const text = e.target.value;
-    
-    setSearchText(text)
-
-    setIsFiltered(false)
-
-    setFilteredStudents(students.filter((student) =>
-      student.name.toLowerCase().includes(text.toLowerCase())
-    ));
-  }
+  useEffect(() => {
+    setFilteredStudents(students.filter(student => 
+      (!isFiltered || student.attendance === "Regular") && 
+      student.name.toLowerCase().includes(searchText.toLowerCase())))
+  }, [isFiltered, searchText])
 
   return (
     <>
-    {isLoading ?   (
-        <p>Cargando...</p>
-      ) : 
-      filteredStudents.length > 0 ? <>
-          <h3>A continuación se mostrará el listado de alumnos</h3>
-          <button onClick={onFilterClick}>{isFiltered ? 'Quitar filtro' : 'Filtrar'} alumnos libres</button>
-          <input type="text" value={searchText} onChange={handleInputChange} placeholder='Ingrese nombre/apellido a buscar'/>
-          <ul>
-          {filteredStudents.map(student => <Student {...student}/>) }
-          </ul>
-          <p>La cantidad total de alumnos es: {filteredStudents.length}</p>
-        </>
-        :
-        <h3>No se encontraron alumnos para este curso!</h3>
+    {isLoading ? 
+      <p>Cargando...</p> :
+      <>
+        <StudentsFilters 
+          isFiltered={isFiltered}
+          setIsFiltered={setIsFiltered}
+          searchText={searchText}
+          setSearchText={setSearchText}/>
+          
+        <StudentsTable students={filteredStudents}/>
+      </>
       }
     </>
   )
