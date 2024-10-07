781ae17423bbff8aef94d23ece3234f9755215cc
GASTON LARRIERA <larriera.gaston@gmail.com>
2024-10-07 00:11:31 -0300

    Install prettier and add script to format

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { Home } from './modules/home';
-import { StudentsList } from './modules/students';
-import { Contact } from './modules/contact';
-import { About } from './modules/about';
-import { Layout } from './modules/layout';
+import { Home } from './modules/home'
+import { StudentsList } from './modules/students'
+import { Contact } from './modules/contact'
+import { About } from './modules/about'
+import { Layout } from './modules/layout'
 
-export const App = () => 
-(<div className="App">
+export const App = () => (
+  <div className="App">
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />} >
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/students" element={<StudentsList />} />
           <Route path="/contact" element={<Contact />} />
@@ -18,4 +17,5 @@ export const App = () =>
         </Route>
       </Routes>
     </BrowserRouter>
-  </div>)
+  </div>
+)
